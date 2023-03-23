646efdc6c15eff2d04b84939f0fd7930f88c54a7
george-koshil <66992395+george-koshil@users.noreply.github.com>
2023-03-23 20:04:37 +0200

    Add cors mode

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,4 +1,6 @@
-const BASE_URL = process.env.API_BASE_URL ?? 'https://digital-school-server.netlify.app/.netlify/functions/api'
+const BASE_URL =
+  process.env.API_BASE_URL ??
+  "https://digital-school-server.netlify.app/.netlify/functions/api";
 
 export interface HttpResponse<T> extends Response {
   parsedBody?: T;
@@ -6,34 +8,60 @@ export interface HttpResponse<T> extends Response {
 
 export class HttpClient {
   public baseUrl: string;
-  public headers: HeadersInit | undefined
+  public headers: HeadersInit | undefined;
 
   constructor(baseUrl: string, headers?: HeadersInit) {
     this.baseUrl = baseUrl;
-    this.headers = headers ?? {}
+    this.headers = headers ?? {};
   }
 
-  async request<T>(url: string, options?: RequestInit): Promise<HttpResponse<T>> {
-    const response: HttpResponse<T> = await fetch(this.baseUrl + url, { headers: { 'Content-Type': 'application/json', ...this.headers }, mode: 'no-cors', ...options});
+  async request<T>(
+    url: string,
+    options?: RequestInit
+  ): Promise<HttpResponse<T>> {
+    const response: HttpResponse<T> = await fetch(this.baseUrl + url, {
+      headers: { "Content-Type": "application/json", ...this.headers },
+      mode: "cors",
+      ...options,
+    });
     response.parsedBody = await response.json();
     return response;
   }
 
   async get<T>(url: string, options?: RequestInit): Promise<HttpResponse<T>> {
-    return await this.request<T>(url, { ...options, method: 'GET' });
+    return await this.request<T>(url, { ...options, method: "GET" });
   }
 
-  async post<T>(url: string, body: any, options?: RequestInit): Promise<HttpResponse<T>> {
-    return await this.request<T>(url, { ...options, method: 'POST', body: JSON.stringify(body) });
+  async post<T>(
+    url: string,
+    body: any,
+    options?: RequestInit
+  ): Promise<HttpResponse<T>> {
+    return await this.request<T>(url, {
+      ...options,
+      method: "POST",
+      body: JSON.stringify(body),
+    });
   }
 
-  async put<T>(url: string, body: any, options?: RequestInit): Promise<HttpResponse<T>> {
-    return await this.request<T>(url, { ...options, method: 'PUT', body: JSON.stringify(body) });
+  async put<T>(
+    url: string,
+    body: any,
+    options?: RequestInit
+  ): Promise<HttpResponse<T>> {
+    return await this.request<T>(url, {
+      ...options,
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
   }
 
-  async delete<T>(url: string, options?: RequestInit): Promise<HttpResponse<T>> {
-    return await this.request<T>(url, { ...options, method: 'DELETE' });
+  async delete<T>(
+    url: string,
+    options?: RequestInit
+  ): Promise<HttpResponse<T>> {
+    return await this.request<T>(url, { ...options, method: "DELETE" });
   }
 }
 
-export const http = new HttpClient(BASE_URL)
+export const http = new HttpClient(BASE_URL);
