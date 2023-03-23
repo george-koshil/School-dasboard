3f630bb70f93055a4d878ca02d861a3c3d2fd59c
george-koshil <66992395+george-koshil@users.noreply.github.com>
2023-03-23 19:09:25 +0200

    Update http.service.ts

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.API_BASE_URL ?? 'http://localhost:3000'
+const BASE_URL = process.env.API_BASE_URL ?? 'https://digital-school-server.netlify.app'
 
 export interface HttpResponse<T> extends Response {
   parsedBody?: T;
